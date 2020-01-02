89305c7b358d40f4780e2a59effd56b85ffa8d82
Vlad Keliy <limonkeliy153@gmail.com>
2020-01-02 18:20:14 +0300

    :sparkles: Add whoami ednpoint

--- src/server.ts
+++ src/server.ts
@@ -7,19 +7,18 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.use(cors({ optionsSuccessStatus: 200 }));
 app.use(express.static(path.join(__dirname, 'public')));
-
-// http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
-// http://expressjs.com/en/starter/basic-routing.html
 app.get('/', function(_, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-// your first API endpoint...
-app.get('/api/hello', function(_, res) {
-  res.json({ greeting: 'hello API' });
+app.get('/api/whoami', function(req, res) {
+  res.json({
+    ipaddress: req.connection.remoteAddress,
+    language: req.header('accept-language'),
+    software: req.header('user-agent'),
+  });
 });
 
-// listen for requests :)
 app.listen(process.env.PORT || 3000);
